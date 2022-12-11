6150b81782f39473835a7176fb7a3cf621a62fd7
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2022-12-11 18:44:20 +0900

    feat: giscus 연동

---
+++ .vuepress/components/Comment.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="comments"></div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.initGiscus();
+  },
+  methods: {
+    initGiscus(){
+      const dom = window.document.getElementById('comments');
+      const giscus = document.createElement("script");
+      giscus.type = "text/javascript";
+      giscus.async = true;
+      giscus.crossorigin = "anonymous";
+      giscus.src = "https://giscus.app/client.js";
+
+      giscus.setAttribute("data-repo", "101front/101-Front-Study");
+      giscus.setAttribute("data-theme", "light");
+
+      if (dom.hasChildNodes()) {
+        dom.innerHTML = '';
+      }
+
+      dom.appendChild(giscus) 
+    }
+  }
+}
+</script>
---
+++ .vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,36 @@
+<template>
+    <ParentLayout >
+        <template #page-bottom>
+            <div id="comment-container">
+                <div id="divider" />
+                <Comment class="content" v-bind:style="{width:'55%'}"/>
+            </div>
+        </template>
+    </ParentLayout>
+</template>
+
+<script>
+    import ParentLayout from '@parent-theme/layouts/Layout.vue'
+    import Comment from '../../components/Comment'
+    export default {
+        components: {
+            ParentLayout,
+            Comment
+        }
+    }
+</script>
+
+<style>
+#comment-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+}
+
+#divider {
+    border-bottom: 1px solid #eaecef;
+    width: 55%;
+    margin: 3rem 0;
+}
+</style>
